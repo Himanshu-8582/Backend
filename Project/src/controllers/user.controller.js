ab0a7f76173d55dbdf01e30ea81bf88f182b7cba
Himanshu <himanshugupta8582@gmail.com>
2025-12-25 12:11:19 +0530

    learned about aggregation pipeline and implement getUserChannelProfile in userController

--- Project/src/controllers/user.controller.js
+++ Project/src/controllers/user.controller.js
@@ -198,7 +198,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     if (!email || !fullName) {
         throw new ApiError(400, 'Email and Full Name are required');
     }
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user._id,
         {
             $set: {
@@ -259,8 +259,78 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, 'User coverImage updated successfully', user));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => { 
+    const { username } = req.params;
+    if (!username?.trim()) {
+        throw new ApiError(400, 'Username is required');
+    }
+
+    // aggregation to get user profile along with subscribers and subscribed channels ( aggregation framework/pipeline )
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: 'subscriptions',
+                localField: '_id',
+                foreignField: 'channel',
+                as: 'subscribers'
+            }
+        },
+        {
+            $lookup: {
+                from: 'subscriptions',
+                localField: '_id',
+                foreignField: 'subscriber',
+                as: 'subscribedTo'
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: '$subscribers'
+                },
+                channelsSubscribedToCount: {
+                    $size: '$subscribedTo'
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, '$subscribers.subscriber']
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+    ]);
+
+    if (!channel?.length === 0) {
+        throw new ApiError(404, 'User channel does not exist');
+    }
+    return res.status(200).json(new ApiResponse(200, 'User channel profile fetched successfully', channel[0]));
+
+});
+
+
 export {
     registerUser, loginUser, logoutUser, refreshAccessToken,
     changeCurrentPassword, getCurrentuser, updateAccountDetails,
-    updateUserAvatar, updateUserCoverImage
+    updateUserAvatar, updateUserCoverImage, getUserChannelProfile
 };
